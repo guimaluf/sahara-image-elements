782cbf03bbe06ab627f7a4814a94a6d78f9be57f
Guilherme Maluf <guimalufb@gmail.com>
2018-09-26 12:57:58 -0300

    Hardcode sparkbench directory
    Change-Id: I60dbc399443ee64d8a2e50f7584643ea37794912

--- diskimage-create/diskimage-create.sh
+++ diskimage-create/diskimage-create.sh
@@ -411,6 +411,7 @@ if need_required_packages; then
                     sudo rpm -Uvh --force http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
                 fi
                 sudo yum install $package_list -y
+                ;;
             "gentoo")
                 sudo emerge -vq $package_list
                 ;;
